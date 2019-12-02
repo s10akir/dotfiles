751201667e0f41f9bd735bfc195e6fffc84a6974
Akira Shinohara <k017c1067@it-neec.jp>
2019-12-03 08:27:35 +0900

    update moromoro

--- src/.config/polybar/launch.sh
+++ src/.config/polybar/launch.sh
@@ -7,6 +7,8 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch
-polybar main &
+for monitor in `xrandr -q | grep " connected" | cut -d ' ' -f1`; do
+  MONITOR=$monitor polybar main &
+done
 
 echo "Bar launched..."
