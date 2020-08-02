b1ed041e2d374c1aee6d1a438c096ac0d02f54fb
Akira Shinohara <akira.shinohara@mojamoja.cloud>
2020-08-02 09:13:30 +0000

    feat: update init script

--- init.sh
+++ init.sh
@@ -1,26 +1,28 @@
-#! /bin/sh
+#! /bin/bash
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 
 # TODO: /.config に入っていない dotfiles は個別に貼ってる
 #       ディレクトリを無視するループを書けばいいだけなのでそのうちやる
-ln -sfv $SCRIPT_DIR/src/.gitconfig $HOME
-ln -sfv $SCRIPT_DIR/src/.tmux.conf $HOME
+ln -snfv $SCRIPT_DIR/src/.gitconfig $HOME
+ln -snfv $SCRIPT_DIR/src/.tmux.conf $HOME
 
 
 ONLY_LINUX="^(compton|i3|polybar)$"
 
 for f in `ls $SCRIPT_DIR/src/.config/`; do
+  rm -rf $HOME/.config/$f # DANGER
+
   if [[ $f =~ $ONLY_LINUX ]]; then
     if [ "$(uname)" == 'Darwin' ]; then
       echo "[skip] $f is only linux config"
     else
       # Linuxのみ
-      ln -sfv $SCRIPT_DIR/src/.config/$f $HOME/.config/
+      ln -snfv $SCRIPT_DIR/src/.config/$f $HOME/.config/
     fi
   else
     # mac Linux共通
-    ln -sfv $SCRIPT_DIR/src/.config/$f $HOME/.config/
+    ln -snfv $SCRIPT_DIR/src/.config/$f $HOME/.config/
   fi
 done
