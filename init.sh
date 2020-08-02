71bc4042365b4b1865810ea9fb871bcb8ccff95d
Akira Shinohara <akira.shinohara@mojamoja.cloud>
2020-08-02 18:09:09 +0900

    feat: update init script

--- init.sh
+++ init.sh
@@ -2,35 +2,25 @@
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-# applications
 
-# zsh
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+# TODO: /.config に入っていない dotfiles は個別に貼ってる
+#       ディレクトリを無視するループを書けばいいだけなのでそのうちやる
+ln -sfv $SCRIPT_DIR/src/.gitconfig $HOME
+ln -sfv $SCRIPT_DIR/src/.tmux.conf $HOME
 
-ln -sf $SCRIPT_DIR/src/.zlogin $HOME
-ln -sf $SCRIPT_DIR/src/.zlogout $HOME
-ln -sf $SCRIPT_DIR/src/.zpreztorc $HOME
-ln -sf $SCRIPT_DIR/src/.zprofile $HOME
-ln -sf $SCRIPT_DIR/src/.zshenv $HOME
-ln -sf $SCRIPT_DIR/src/.zshrc $HOME
 
-# nvim
-mkdir -p $HOME/.config/nvim
-ln -sf $SCRIPT_DIR/src/.config/nvim/init.vim $HOME/.config/nvim/
-ln -sf $SCRIPT_DIR/src/.config/nvim/dein.toml $HOME/.config/nvim/
+ONLY_LINUX="^(compton|i3|polybar)$"
 
-# i3
-mkdir -p $HOME/.config/i3
-ln -sf $SCRIPT_DIR/src/.config/i3/config $HOME/.config/i3/
-
-# polybar
-mkdir -p $HOME/.config/polybar
-ln -sf $SCRIPT_DIR/src/.config/polybar/config $HOME/.config/polybar/
-ln -sf $SCRIPT_DIR/src/.config/polybar/launch.sh $HOME/.config/polybar/
-
-# compton
-mkdir -p $HOME/.config/compton
-ln -sf $SCRIPT_DIR/src/.config/compton/compton.conf $HOME/.config/compton/
-
-# tmux
-ln -sf $SCRIPT_DIR/src/.tmux.conf $HOME
+for f in `ls $SCRIPT_DIR/src/.config/`; do
+  if [[ $f =~ $ONLY_LINUX ]]; then
+    if [ "$(uname)" == 'Darwin' ]; then
+      echo "[skip] $f is only linux config"
+    else
+      # Linuxのみ
+      ln -sfv $SCRIPT_DIR/src/.config/$f $HOME/.config/
+    fi
+  else
+    # mac Linux共通
+    ln -sfv $SCRIPT_DIR/src/.config/$f $HOME/.config/
+  fi
+done
