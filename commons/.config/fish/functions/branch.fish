aa37316052dadc1322a191d2aad33147ea55f7fa
Akira Shinohara <akira.shinohara@mojamoja.cloud>
2023-04-20 19:05:56 +0900

    feat: update branch function

--- commons/.config/fish/functions/branch.fish
+++ commons/.config/fish/functions/branch.fish
@@ -1,4 +1,8 @@
 # Defined in - @ line 1
-function branch --wraps='git branch -a | fzf | xargs git switch' --description 'alias branch git branch -a | fzf | xargs git switch'
-  git branch -a | fzf | xargs git switch $argv;
+function branch --wraps='git branch | fzf | xargs git switch' --description 'alias branch git branch -a | fzf | xargs git switch'
+  if [ -z "$argv" ];
+    set argv '';
+  end
+
+  git branch | fzf -q $argv | xargs git switch;
 end
